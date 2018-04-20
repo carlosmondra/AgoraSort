56fa4ef1301f598f3eded128fa67fc65bc496132
Carlos Mondragon <mondragon52369@hotmail.com>
2018-04-19 23:42:20 -0400

    Connection to DB is successful

--- index.php
+++ index.php
@@ -167,6 +167,32 @@ body{
 		
 </div>
 
+<?php
+   class MyDB extends SQLite3 {
+      function __construct() {
+         $this->open('agora.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db) {
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully<br>";
+   }
+
+   $sql =<<<EOF
+      SELECT * from PHONES;
+EOF;
+
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+      echo "ID = ". $row['ID'] . "<br>";
+      echo "PHONE NAME = ". $row['PHONE_NAME'] ."<br><br>";
+   }
+   echo "Operation done successfully\n";
+   $db->close();
+?>
+
 
 </body>
 </html>
