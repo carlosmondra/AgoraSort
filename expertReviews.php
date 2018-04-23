72ef837d6dfaad2c5cd6118b0bcd54693b6cc382
Carlos Mondragon <mondragon52369@hotmail.com>
2018-04-23 19:25:25 -0400

    Add expert reviews

--- expertReviews.php
+++ expertReviews.php
@@ -8,15 +8,23 @@
 <div class="list-group">
     <?php 
         for ($x = 1; $x < $cols; $x++) {
+            $rating = pg_fetch_result($resRatings, 0, $x);
+            $summary = pg_fetch_result($resSummaries, 0, $x);
+            if ($rating and $summary) {
     ?>
-            <div class="list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small><?php echo pg_fetch_result($resRatings, 0, $x); ?></small>
+                <div class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <?php if ($rating) { ?>
+                            <small><?php echo "Score " . $rating . "%"; ?></small>
+                        <?php } ?>
+                    </div>
+                    <?php if ($summary) { ?>
+                        <p class="mb-1"><?php echo $summary; ?></p>
+                    <?php } ?>
                 </div>
-                <p class="mb-1"><?php echo pg_fetch_result($resSummaries, 0, $x); ?></p>
-            </div>
     <?php
+            }
         } 
     ?>
 </div>
