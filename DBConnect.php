d902877d09201501f1a9738feef9cbdbb8b81c9b
Carlos Mondragon <mondragon52369@hotmail.com>
2018-04-23 16:29:05 -0400

    Testing to heroku

--- DBConnect.php
+++ DBConnect.php
@@ -1,5 +1,5 @@
 <?php
-    // $connStr = "host=ec2-107-20-249-68.compute-1.amazonaws.com port=5432 dbname=dakcp6lqfs304k user=uubitbcpbabsnr password=88f000ab5517cb1e1744854b9724a6e98c0c5254ab5d504db852e35fdea1a56a";
-    $connStr = "host=localhost port=5432 dbname=agorasort user=postgres password=mondra";
+    $connStr = "host=ec2-107-20-249-68.compute-1.amazonaws.com port=5432 dbname=dakcp6lqfs304k user=uubitbcpbabsnr password=88f000ab5517cb1e1744854b9724a6e98c0c5254ab5d504db852e35fdea1a56a";
+    // $connStr = "host=localhost port=5432 dbname=agorasort user=postgres password=mondra";
     $conn = pg_connect($connStr);
 ?>
